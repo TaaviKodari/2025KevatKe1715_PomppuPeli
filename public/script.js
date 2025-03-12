27a376b9afbedbc9a14b036361ab3eb38434fdf8
TaaviKodari <taavi.makela@gmail.com>
2025-03-12 18:16:50 +0200

    luokka ja liike X tehty

--- public/script.js
+++ public/script.js
@@ -2,13 +2,19 @@ let pelinKorkeus;
 let pelinLeveys;
 
 let taustakuva;
+let pelastettavakuva;
 
 let mailanYSijainti;
 let mailanKorkeus = 20;
 let mailanLeveys = 40;
 
+//väliaikainen, poista
+let pelastettava;
+let olio2;
+
 function preload(){
     taustakuva = loadImage('kuvat/tausta.png');
+    pelastettavakuva = loadImage('kuvat/pelastettava.png');
 }
 
 function setup(){
@@ -17,15 +23,37 @@ function setup(){
 
     mailanYSijainti = pelinKorkeus - mailanKorkeus;
     createCanvas(pelinLeveys,pelinKorkeus);
+
+    //väliaikainen
+    pelastettava = new Pelastettava();
+    olio2 = new Pelastettava();
 }
 
 function draw(){
     background("black");
-    image(taustakuva,0,0,pelinLeveys,pelinKorkeus);    
+    image(taustakuva,0,0,pelinLeveys,pelinKorkeus);
+   //image(pelastettavakuva,50,50,50,50);    
+    pelastettava.liiku();
+    olio2.liiku();
     piirraMaila();
 }
 
 function piirraMaila(){
     rect(mouseX,mailanYSijainti,mailanLeveys,mailanKorkeus);
 
+}
+
+class Pelastettava{
+    constructor(){
+        this.X = -10;
+        this.Xnopeus = 1;
+        this.Y = 40;
+
+        this.size = pelinKorkeus / 10;
+    }
+
+    liiku(){
+        this.X = this.X + this.Xnopeus;
+        image(pelastettavakuva,this.X, this.Y,this.size,this.size);
+    }
 }
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-import expres from 'express';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+    console.log(`server running at http://localhost:${port}`);
+});
