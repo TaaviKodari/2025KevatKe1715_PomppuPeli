d3cc1bfa4a1a6cd2543bc9d3e495edb8ee7ff429
TaaviKodari <taavi.makela@gmail.com>
2025-03-26 18:17:31 +0200

    olioiden liikuttelu tehty

--- public/script.js
+++ public/script.js
@@ -8,9 +8,8 @@ let mailanYSijainti;
 let mailanKorkeus = 20;
 let mailanLeveys = 40;
 
-//väliaikainen, poista
-let pelastettava;
-let olio2;
+let olioLista = [];
+let olioAjastin;
 
 function preload(){
     taustakuva = loadImage('kuvat/tausta.png');
@@ -23,18 +22,14 @@ function setup(){
 
     mailanYSijainti = pelinKorkeus - mailanKorkeus;
     createCanvas(pelinLeveys,pelinKorkeus);
-
-    //väliaikainen
-    pelastettava = new Pelastettava();
-    olio2 = new Pelastettava();
+    luoOlio();
 }
 
 function draw(){
     background("black");
     image(taustakuva,0,0,pelinLeveys,pelinKorkeus);
    //image(pelastettavakuva,50,50,50,50);    
-    pelastettava.liiku();
-    olio2.liiku();
+    liikutaOlioita();
     piirraMaila();
 }
 
@@ -43,17 +38,45 @@ function piirraMaila(){
 
 }
 
+function luoOlio(){
+    let uusiOlio = new Pelastettava();
+    olioLista.unshift(uusiOlio);
+    olioAjastin = setTimeout(luoOlio,2000);
+}
+
+function liikutaOlioita(){
+    olioLista.forEach(function(olio,index){
+        olio.liiku();
+
+        if(olio.Y > pelinKorkeus){
+            olioLista.splice(index,1);
+        }
+
+    });
+}
+
 class Pelastettava{
     constructor(){
         this.X = -10;
-        this.Xnopeus = 1;
-        this.Y = 40;
-
+        this.Xnopeus = random(1,5);
+        this.Y = random(0,50);
         this.size = pelinKorkeus / 10;
+        this.Ynopeus = -2.5;
+        this.painovoima = 0.05;
     }
 
     liiku(){
-        this.X = this.X + this.Xnopeus;
+        this.X += this.Xnopeus;
+
+        this.Ynopeus += this.painovoima;
+
+        if(this.Y + this.size / 2 > mailanYSijainti){
+            if(this.X > mouseX && this.X < mouseX + mailanLeveys){
+                this.Ynopeus = -abs(this.Ynopeus);
+            }
+        }
+
+        this.Y += this.Ynopeus;
         image(pelastettavakuva,this.X, this.Y,this.size,this.size);
     }
 }
